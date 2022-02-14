be8166ca78eb8e3cc61751b981938576608e9656
Yuhyeingjoo <yhjoo0623@naver.com>
2022-02-14 11:12:19 +0900

    clean up code

--- src/main.rs
+++ src/main.rs
@@ -16,23 +16,17 @@ async fn main() {
 
     let h1 = tokio::spawn(async move{
         let a = lock.lock().await;
-        println!("h1 {:?}", thread::current().id());
         thread::sleep(Duration::from_millis(2000));
         let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
         let (socket, _) = listener.accept().await.unwrap();
-        println!("h1 isetne {:?}", thread::current().id());
         let b = lock4.lock().await;
-        println!("h1 get second lock {:?}", thread::current().id());
     });
 	let h2 = tokio::spawn(async move{
         let a = lock3.lock().await;
-        println!("h2 {:?}", thread::current().id());
         thread::sleep(Duration::from_millis(2000));
         let listener = TcpListener::bind("127.0.0.1:6380").await.unwrap();
         let (socket, _) = listener.accept().await.unwrap();
-        println!("h2 isetne {:?}", thread::current().id());
         let b = lock2.lock().await;
-        println!("h2 get second lock {:?}", thread::current().id());
     });
     let h3 = tokio::spawn(async move{
         thread::sleep(Duration::from_millis(1000));
@@ -42,4 +36,5 @@ async fn main() {
     });
 	h1.await;
 	h2.await;
+    h3.await;
 }
